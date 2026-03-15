eaa363682e3ff99a74483e6d2c8bfc5f93b8d445
manuellla lopes <manuellaggomeslopes@gmail.com>
2026-03-14 22:12:37 -0300

    Exercício simples de conversão de temperatura em Python

---
+++ exercicios/exercicio07/exercicio07.py
@@ -0,0 +1,7 @@
+print("Conversor de Temperatura")
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperatura em Fahrenheit:", fahrenheit)
